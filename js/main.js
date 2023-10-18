b5175a9c0489307494cecea741f28ff35c95c3e6
Keratry <nicolas.keratry@gmail.com>
2023-10-18 23:25:50 +0600

    fix: js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 document.querySelectorAll('li span').forEach(function (link) {
     link.addEventListener('click', function () {
         this.parentElement.classList.toggle('opened');
-        toggle(this.nextElementSibling);
+        
     });
 });
-
-
-function toggle(el) {
-    el.style.display = (el.style.display == 'none') ? '' : 'none'
-}
