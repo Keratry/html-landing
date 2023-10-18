5a3f157d89a676284ffbdb2731e2cb7eb046be39
Keratry <nicolas.keratry@gmail.com>
2023-10-18 23:26:10 +0600

    add js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('li span').forEach(function (link) {
     link.addEventListener('click', function () {
         this.parentElement.classList.toggle('opened');
-        
     });
 });
